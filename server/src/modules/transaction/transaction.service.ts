84a5670c47e35b530e2fbd19d135bad29a887292
Kerelose <keromalak52@outlook.com>
2022-01-01 19:20:01 +0200

    internal transaction updated

--- server/src/modules/transaction/transaction.service.ts
+++ server/src/modules/transaction/transaction.service.ts
@@ -27,78 +27,18 @@ export class TransactionService {
   }
 
   async InternalTransaction(transactionDto : transactionDto) {
-    let transaction = new this.transactionModel(transactionDto);
     let sender = await this.accountService.findAccountByNumber(transactionDto.accountNumberSender);
     let receiver = await this.accountService.findAccountByNumber(transactionDto.accountNumberReceiver);
-
-    sender.updateBalance(-1 * transactionDto.totalAmount);
-    receiver.updateBalance(transactionDto.totalAmount);
+    if(sender.balance < transactionDto.totalAmount){
+      alert("transaction amount is bigger than your balance");
+    }
+    else{
+      sender.updateBalance(-1 * transactionDto.totalAmount);
+      receiver.updateBalance(transactionDto.totalAmount);
+    }
+    let transaction = new this.transactionModel(transactionDto);
 
     return await transaction.save();
   }
 
 }
-
-    
-   /*
-    _isPositive(amount: number): boolean{
-      
-      const isPositive = amount > 0;
-      if(!isPositive){
-        console.error('Amount Must be Positive');
-        return false;
-      }
-      return true;
-    }
-
-    _isAllowed(amount: number , account : accountDto): boolean{
-      if(!this._isPositive(amount))
-        return false;
-      const isAllowed = account.totalAmount - amount >= 0;
-      if(!isAllowed){
-        console.error('You have insufficient funds!');
-        return false;
-      }
-      return true;  
-    }
-  
-    getLedgerDate(account : Account): string {
-
-      let options: Intl.DateTimeFormatOptions = {
-          day: "numeric", month: "numeric", year: "numeric",
-          hour: "2-digit", minute: "2-digit"
-      };
-
-      return account.date.toLocaleDateString("en-GB", options) + " " + account.date.toLocaleTimeString("en-GB", options);
-   }
-   
-   getTheDeposite(amount : number , account : Account): boolean {
-     if(this._isPositive(amount)){
-       account.balance += amount;
-       console.info(`Deposite: ${account.active , account.id} new balance is ${account.balance}`);
-       return true;
-     }
-     return false;
-   }
-   withDraw(amount : number , account : accountDto ) : boolean{
-     if(this._isAllowed(amount , account)){
-        account.totalAmount -= amount;
-       console.info(`Withdraw: ${this.accountModel , this.accountModel} new balance is ${this.accountModel}`)
-       return true;
-     }
-     return false;
-   }
-   MakeTransaction(amount: number , account : accountDto) : boolean{ 
-     this.withDraw(amount , account); 
-     account.totalAmount += amount;
-     return true; 
-   }*/
-   /*function Account(name:string , balance : number) {
-
-     this.name = name;
-     this.balance = balance;
-     
-   }*/
-
-
-  // TODO: Define your Transaction Service Logic
