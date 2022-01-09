c93ffa823780238511b7876c636dfac724a0bad2
Abdelrahman Hany <34286982+Karitori@users.noreply.github.com>
2022-01-09 21:08:15 +0200

    minor tweaks

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -2,17 +2,19 @@ import "../styles/globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { QueryClient, QueryClientProvider, useQuery } from "react-query";
 import Navbar from "../components/Navbar";
+import background from "../public/Logomaske_384x215_blue_light.jpg"
 
 const queryClient = new QueryClient();
-
-
+const URL = background
 
 function MyApp({ Component, pageProps }) {
-    return (
-      <QueryClientProvider client={queryClient}>
-        <Component {...pageProps} />
-      </QueryClientProvider>
-    );
+	return (
+		<div style={{backgroundImage: URL}}>
+			<QueryClientProvider client={queryClient}>
+				<Component {...pageProps} />
+			</QueryClientProvider>
+		</div>
+	);
 }
 
 export default MyApp;
