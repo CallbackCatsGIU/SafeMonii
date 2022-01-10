8464afee5d3044212b168bc8b277764c56096032
mk-elbaz <66201789+mk-elbaz@users.noreply.github.com>
2022-01-10 15:24:37 +0200

    Update _app.js
    Co-Authored-By: Youssef El Zayat <82692972+youssefelzayat@users.noreply.github.com>

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -3,13 +3,18 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { QueryClient, QueryClientProvider, useQuery } from "react-query";
 import Navbar from "../components/Navbar";
 import background from "../public/Logomaske_384x215_blue_light.jpg"
+import Head from 'next/head'
 
 const queryClient = new QueryClient();
 const URL = background
 
+
 function MyApp({ Component, pageProps }) {
 	return (
 		<div style={{backgroundImage: URL}}>
+			<Head>
+		  <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+		</Head>
 			<QueryClientProvider client={queryClient}>
 				<Component {...pageProps} />
 			</QueryClientProvider>
